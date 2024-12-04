547e1427d96f6401cc5c40f6ad18f0afb4f179da
Sobirov S <sobiroff23@gmail.com>
2024-12-04 11:08:04 +0500

    fixed

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="items-center justify-center text-center pt-[25%]">
   <h1 class=" text-5xl font-bold underline">
-    Hello world!
+    Hello WORLD and Vue!
   </h1>
 </div>
 </template>
